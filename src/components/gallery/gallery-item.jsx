7dac8de1aee5a476228c3e2cf0f558d7b26451c9
Lars Knoke <larz.knoke@gmail.com>
2024-12-05 22:41:50 +0100

    gallery item fix

--- src/components/gallery/gallery-item.jsx
+++ src/components/gallery/gallery-item.jsx
@@ -1,7 +1,7 @@
 function GalleryItem({ img }) {
   return (
     <div className="relative gallery-item">
-      <img className="max-w-full  object-cover" src={img} alt="" />
+      <img className="max-w-full md:h-full object-cover" src={img} alt="" />
       <div className="gallery-caption">Bildunterschrift</div>
     </div>
   );
