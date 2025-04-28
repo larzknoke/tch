c17c42c1c3683340083e637b0129748889bc0937
Lars Knoke <l.knoke@colorplus.de>
2025-04-28 07:54:24 +0200

    class -> className

--- src/components/ui/loading-ball.jsx
+++ src/components/ui/loading-ball.jsx
@@ -1,9 +1,9 @@
 export default function BallLoader(props) {
   const color = props.color || "text-white";
   return (
-    <div class="tennis-loading">
-      <div class="shadow"></div>
-      <div class="tennis-ball"></div>
+    <div className="tennis-loading">
+      <div className="shadow"></div>
+      <div className="tennis-ball"></div>
     </div>
   );
 }
