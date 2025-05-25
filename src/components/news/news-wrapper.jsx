771df12f833695acd20d9bbac31d25e14cf518e7
Lars Knoke <larz.knoke@gmail.com>
2025-05-25 22:27:00 +0200

    sort news

--- src/components/news/news-wrapper.jsx
+++ src/components/news/news-wrapper.jsx
@@ -8,9 +8,11 @@ function NewsWrapper() {
     <div className="px-5 py-5 md:py-0 md:px-0">
       <HeaderText text="NEWS & NEUIGKEITEN" />
       <div className="pt-3 grid gap-8 grid-cols-1 md:grid-cols-2">
-        {newsData.map((news) => (
-          <NewsItem key={news.id} news={news} />
-        ))}
+        {newsData
+          .sort((a, b) => b.id - a.id)
+          .map((news) => (
+            <NewsItem key={news.id} news={news} />
+          ))}
       </div>
     </div>
   );
