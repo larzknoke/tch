ede221e7e90f77c718768c592bd01f245411679d
Lars Knoke <larz.knoke@gmail.com>
2025-07-25 06:51:52 +0200

    CKE

--- src/components/ui/admin/ckeditor-admin.js
+++ src/components/ui/admin/ckeditor-admin.js
@@ -9,9 +9,6 @@ import { Roboto_Condensed } from "next/font/google";
 
 // import "./App.css";
 
-const LICENSE_KEY =
-  "eyJhbGciOiJFUzI1NiJ9.eyJleHAiOjE3NTQ2MTExOTksImp0aSI6ImM1N2ZjZDU3LWUwMjAtNDVhMy1hNWQyLTE3YTc2ODc3MjNhMSIsInVzYWdlRW5kcG9pbnQiOiJodHRwczovL3Byb3h5LWV2ZW50LmNrZWRpdG9yLmNvbSIsImRpc3RyaWJ1dGlvbkNoYW5uZWwiOlsiY2xvdWQiLCJkcnVwYWwiLCJzaCJdLCJ3aGl0ZUxhYmVsIjp0cnVlLCJsaWNlbnNlVHlwZSI6InRyaWFsIiwiZmVhdHVyZXMiOlsiKiJdLCJ2YyI6IjEwMmNkYmUxIn0.Vy-gI6GDHZvj8I-yagVvJoxVCnW2UsowBh5Ga6DPRgabT5Y8XYvL-aEK17Uo9JF_xzwesFEoFMXA0UPjEEDRig";
-
 const roboto_cond = Roboto_Condensed({ subsets: ["latin"] });
 
 export default function CKEditorAdmin() {
@@ -38,6 +35,7 @@ export default function CKEditorAdmin() {
       AutoImage,
       AutoLink,
       Autosave,
+      BalloonToolbar,
       Bold,
       CloudServices,
       Code,
@@ -50,6 +48,7 @@ export default function CKEditorAdmin() {
       ImageBlock,
       ImageCaption,
       ImageInline,
+      ImageInsert,
       ImageInsertViaUrl,
       ImageResize,
       ImageStyle,
@@ -65,6 +64,7 @@ export default function CKEditorAdmin() {
       ListProperties,
       Paragraph,
       ShowBlocks,
+      SimpleUploadAdapter,
       Table,
       TableCaption,
       TableCellProperties,
@@ -92,6 +92,7 @@ export default function CKEditorAdmin() {
             "|",
             "horizontalLine",
             "link",
+            "insertImage",
             "insertImageViaUrl",
             "insertTable",
             "codeBlock",
@@ -112,6 +113,7 @@ export default function CKEditorAdmin() {
           AutoImage,
           AutoLink,
           Autosave,
+          BalloonToolbar,
           Bold,
           CloudServices,
           Code,
@@ -124,6 +126,7 @@ export default function CKEditorAdmin() {
           ImageBlock,
           ImageCaption,
           ImageInline,
+          ImageInsert,
           ImageInsertViaUrl,
           ImageResize,
           ImageStyle,
@@ -139,6 +142,7 @@ export default function CKEditorAdmin() {
           ListProperties,
           Paragraph,
           ShowBlocks,
+          SimpleUploadAdapter,
           Table,
           TableCaption,
           TableCellProperties,
@@ -146,6 +150,16 @@ export default function CKEditorAdmin() {
           TableProperties,
           TableToolbar,
           TextTransformation,
+        ],
+        balloonToolbar: [
+          "bold",
+          "italic",
+          "|",
+          "link",
+          "insertImage",
+          "|",
+          "bulletedList",
+          "numberedList",
         ],
         heading: {
           options: [
@@ -217,7 +231,7 @@ export default function CKEditorAdmin() {
         initialData:
           '<h2>Congratulations on setting up CKEditor 5! 🎉</h2>\n<p>\n\tYou\'ve successfully created a CKEditor 5 project. This powerful text editor\n\twill enhance your application, enabling rich text editing capabilities that\n\tare customizable and easy to use.\n</p>\n<h3>What\'s next?</h3>\n<ol>\n\t<li>\n\t\t<strong>Integrate into your app</strong>: time to bring the editing into\n\t\tyour application. Take the code you created and add to your application.\n\t</li>\n\t<li>\n\t\t<strong>Explore features:</strong> Experiment with different plugins and\n\t\ttoolbar options to discover what works best for your needs.\n\t</li>\n\t<li>\n\t\t<strong>Customize your editor:</strong> Tailor the editor\'s\n\t\tconfiguration to match your application\'s style and requirements. Or\n\t\teven write your plugin!\n\t</li>\n</ol>\n<p>\n\tKeep experimenting, and don\'t hesitate to push the boundaries of what you\n\tcan achieve with CKEditor 5. Your feedback is invaluable to us as we strive\n\tto improve and evolve. Happy editing!\n</p>\n<h3>Helpful resources</h3>\n<ul>\n\t<li>📝 <a href="https://portal.ckeditor.com/checkout?plan=free">Trial sign up</a>,</li>\n\t<li>📕 <a href="https://ckeditor.com/docs/ckeditor5/latest/installation/index.html">Documentation</a>,</li>\n\t<li>⭐️ <a href="https://github.com/ckeditor/ckeditor5">GitHub</a> (star us if you can!),</li>\n\t<li>🏠 <a href="https://ckeditor.com">CKEditor Homepage</a>,</li>\n\t<li>🧑‍💻 <a href="https://ckeditor.com/ckeditor-5/demo/">CKEditor 5 Demos</a>,</li>\n</ul>\n<h3>Need help?</h3>\n<p>\n\tSee this text, but the editor is not starting up? Check the browser\'s\n\tconsole for clues and guidance. It may be related to an incorrect license\n\tkey if you use premium features or another feature-related requirement. If\n\tyou cannot make it work, file a GitHub issue, and we will help as soon as\n\tpossible!\n</p>\n',
         language: "de",
-        licenseKey: LICENSE_KEY,
+        licenseKey: process.env.NEXT_PUBLIC_CKE_LICENSE_KEY,
         link: {
           addTargetToExternalLinks: true,
           defaultProtocol: "https://",
@@ -254,7 +268,6 @@ export default function CKEditorAdmin() {
       },
     };
   }, [cloud, isLayoutReady]);
-
   return (
     <div className={`main-container ${roboto_cond.className} `}>
       <div
