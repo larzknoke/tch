45e747f26afd78e43b41b9c949f6090411c14c25
Lars Knoke <l.knoke@colorplus.de>
2025-09-18 13:08:58 +0200

    Gallery

--- src/components/gallery/gallery.jsx
+++ src/components/gallery/gallery.jsx
@@ -7,24 +7,32 @@ function Gallery() {
       <HeaderText text="GALLERY" />
       <div className="grid grid-cols-1 md:grid-cols-4 gap-2 md:gap-4">
         <div className="grid gap-4">
-          <GalleryItem img={"images/gallery/gal1.jpg"} />
-          <GalleryItem img={"images/gallery/gal2.jpg"} />
-          <GalleryItem img={"images/gallery/gal3.jpg"} />
+          <GalleryItem img={"news/Sommerfest2025/19.jpg"} />
+          <GalleryItem img={"news/MichaelKoehne800.jpg"} />
+          <GalleryItem img={"news/Sommerfest2025/01.jpg"} />
+          <GalleryItem img={"news/Sommerfest2025/09.jpg"} />
+          <GalleryItem img={"news/Sommerfest2025/04.jpg"} />
         </div>
         <div className="grid gap-4">
-          <GalleryItem img={"images/gallery/gal4.jpg"} />
-          <GalleryItem img={"images/gallery/gal5.jpg"} />
-          <GalleryItem img={"images/gallery/gal6.jpg"} />
+          <GalleryItem img={"news/Sommerfest2025/11.jpg"} />
+          <GalleryItem img={"news/Sommerfest2025/16.jpg"} />
+          <GalleryItem img={"news/Sommerfest2025/14.jpg"} />
+          <GalleryItem img={"news/Sommerfest2025/06.jpg"} />
+          <GalleryItem img={"news/Sommerfest2025/10.jpg"} />
         </div>
         <div className="grid gap-4">
-          <GalleryItem img={"images/gallery/gal7.jpg"} />
-          <GalleryItem img={"images/gallery/gal8.jpg"} />
-          <GalleryItem img={"images/gallery/gal2.jpg"} />
+          <GalleryItem img={"news/Erfolgreicher-Start.jpg"} />
+          <GalleryItem img={"news/blo25_opener.jpg"} />
+          <GalleryItem img={"news/Sommerfest2025/18.jpg"} />
+          <GalleryItem img={"news/Sommerfest2025/22.jpg"} />
+          <GalleryItem img={"news/Sommerfest2025/15.jpg"} />
         </div>
         <div className="grid gap-4">
-          <GalleryItem img={"images/gallery/gal4.jpg"} />
-          <GalleryItem img={"images/gallery/gal5.jpg"} />
-          <GalleryItem img={"images/gallery/gal6.jpg"} />
+          <GalleryItem img={"news/Sommerfest2025/23.jpg"} />
+          <GalleryItem img={"news/Spring-Junior-Challenge-2025.jpg"} />
+          <GalleryItem img={"news/Sommerfest2025/24.jpg"} />
+          <GalleryItem img={"news/Sommerfest2025/12.jpg"} />
+          <GalleryItem img={"news/Sommerfest2025/28.jpg"} />
         </div>
       </div>
     </div>
