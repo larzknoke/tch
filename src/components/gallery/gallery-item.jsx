5713dd2ad1a48d0189ce5becdbe32347858b66aa
Lars Knoke <l.knoke@colorplus.de>
2025-09-18 13:10:43 +0200

    alt=""

--- src/components/gallery/gallery-item.jsx
+++ src/components/gallery/gallery-item.jsx
@@ -1,7 +1,11 @@
 function GalleryItem({ img }) {
   return (
     <div className="relative gallery-item">
-      <img className="max-w-full md:h-full object-cover" src={img} alt="" />
+      <img
+        className="max-w-full md:h-full object-cover"
+        src={img}
+        alt={"Gallery Bild"}
+      />
       <div className="gallery-caption">Bildunterschrift</div>
     </div>
   );
